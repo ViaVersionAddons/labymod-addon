cd94aed5b4db625301d576fb4ea76c7295c1e344
Emmanuel Lampe <mail@emmanuel-lampe.de>
2022-11-19 14:49:07 +0100

    chore: update viaversion and backwards to 4.5.0-22w43a-SNAPSHOT

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -21,8 +21,8 @@ dependencies {
     implementation("com.viaversion:viarewind-fabric:2.0.3-SNAPSHOT")
 
     maven("https://oss.sonatype.org/service/local/staging/deploy/maven2/", "org.yaml:snakeyaml:1.33")
-    maven("https://repo.viaversion.com/", "com.viaversion:viaversion:4.4.3-SNAPSHOT")
-    maven("https://repo.viaversion.com/", "com.viaversion:viabackwards:4.4.2-SNAPSHOT")
+    maven("https://repo.viaversion.com/", "com.viaversion:viaversion:4.5.0-22w43a-SNAPSHOT")
+    maven("https://repo.viaversion.com/", "com.viaversion:viabackwards:4.5.0-22w43a-SNAPSHOT")
     maven("https://repo.viaversion.com/", "com.viaversion:viarewind-fabric:2.0.3-SNAPSHOT")
 
     compileOnly("io.netty:netty-all:4.1.77.Final")
